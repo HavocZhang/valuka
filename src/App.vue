362f54bc3587287688423f62dcb0f8003548e812
张浩杰 <valuka.zhanghaojie@gmail.com>
2022-03-04 10:51:44 +0800

    ✨新增: 测试增加hello

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+console.log('hello')
 </script>
 
 <template>
